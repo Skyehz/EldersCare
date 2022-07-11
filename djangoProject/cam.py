ed98cb9b575f4f98acbe79a3429f4b0b93c5b753
Skyehz <czh15296700133@163.com>
2022-07-11 15:01:11 +0800

    第一次添加算法

---
+++ djangoProject/cam.py
@@ -0,0 +1,83 @@
+import argparse
+from datetime import datetime
+import cv2
+
+
+class VideoCamera(object):
+    def __init__(self):
+        self.cap = cv2.VideoCapture("rtmp://39.107.230.98:1935/live/home")
+        # self.cap = cv2.VideoCapture(0)
+
+    def __del__(self):
+        self.cap.release()
+
+    # 处理画面（画警戒区域等等）
+    def get_frame(self):
+        success, image = self.cap.read()
+        image = cv2.resize(image, (0, 0), fx=0.25, fy=0.25)
+        # if image != None:
+        datet = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        # 标注左上角的时间
+        cv2.putText(image, datet, (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.5,
+                    (255, 255, 255), 2, cv2.LINE_AA)  # cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 2, 8
+
+        # 算法处理
+
+        # keep looping
+        # grab the current frame
+        # (grabbed, frame) = camera.read()
+        #
+        # # if we are viewing a videos and we did not grab a frame, then we
+        # # have reached the end of the videos
+        # if not input_video:
+        #     frame = cv2.flip(frame, 1)
+        #
+        # # resize the frame, convert it to grayscale, and then clone the
+        # # original frame so we can draw on it later in the program
+        # frame = imutils.resize(frame, width=600)
+        #
+        # face_location_list, names = faceutil.get_face_location_and_name(
+        #     frame)
+        # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        #
+        # # loop over the faces bounding boxes
+        # for ((left, top, right, bottom), name) in zip(
+        #         face_location_list,
+        #         names):
+        #     # extract the ROI of the faces from the grayscale image,
+        #     # resize it to a fixed 28x28 pixels, and then prepare the
+        #     # ROI for classification via the CNN
+        #     roi = gray[top:bottom, left:right]
+        #     roi = cv2.resize(roi, (FACIAL_EXPRESSION_TARGET_WIDTH,
+        #                            FACIAL_EXPRESSION_TARGET_HEIGHT))
+        #     roi = roi.astype("float") / 255.0
+        #     roi = img_to_array(roi)
+        #     roi = np.expand_dims(roi, axis=0)
+        #
+        #     # determine facial expression
+        #     (neural, smile) = model.predict(roi)[0]
+        #     label = "Neural" if neural > smile else "Smile"
+        #
+        #     # display the label and bounding box rectangle on the output
+        #     # frame
+        #     cv2.putText(frame, name + " is" + label, (left, top - 10),
+        #                 cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+        #     cv2.rectangle(frame, (left, top), (right, bottom),
+        #                   (0, 0, 255), 2)
+        #
+        # # show our detected faces along with smiling/not smiling labels
+        # cv2.imshow("Facial Expression Detect", frame)
+        #
+        # # Press 'ESC' for exiting videos
+        # k = cv2.waitKey(100) & 0xff
+
+
+        # 画出警戒区域
+        ptLeftTop = (0, 0)
+        ptRightBottom = (80, 80)
+        cv2.rectangle(image, ptLeftTop, ptRightBottom, (255, 0, 0), 2)
+
+        ret, jpeg = cv2.imencode('.jpeg', image)
+        return jpeg.tobytes()
+        # else:
+        #     print("wrong read")
